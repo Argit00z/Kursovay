c8e7651f55996469d95a2414e2b88992f51d6c49
Arman <galstyanarman05@gmail.com>
2024-06-20 13:30:32 +0300

    Rename fxm file and add Authorization view

--- src/main/java/com/example/kursovayapp/AuthorizationApplication.java
+++ src/main/java/com/example/kursovayapp/AuthorizationApplication.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class AuthorizationApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(AuthorizationApplication.class.getResource("Authorization-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 400);
         stage.setTitle("Kursovay");
         stage.setScene(scene);
