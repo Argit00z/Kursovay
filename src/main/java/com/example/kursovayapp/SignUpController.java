b0ef465f3fa88a5de0ee8f75f38b31870b17b0e7
Arman <galstyanarman05@gmail.com>
2024-06-20 14:35:58 +0300

    Add scene SignUp

---
+++ src/main/java/com/example/kursovayapp/SignUpController.java
@@ -0,0 +1,40 @@
+package com.example.kursovayapp;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class SignUpController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button authSiginButton;
+
+    @FXML
+    private PasswordField singUpAdress;
+
+    @FXML
+    private TextField singUpName;
+
+    @FXML
+    private PasswordField singUpPassword;
+
+    @FXML
+    private TextField singUpPhoneNumber;
+
+    @FXML
+    void initialize() {
+
+
+    }
+
+}
+
